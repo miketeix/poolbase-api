9269c33eeac422f5d127bb8526c609a17d6eab0e
Michael Teixeira <meixeira@gmail.com>
2018-07-04 22:07:00 -0400

    Use Pod port from Heroku, fallback to config file port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const logger = require('winston');
 const app = require('./app');
 
-const port = app.get('port');
+const port = process.env.PORT || app.get('port');
 const server = app.listen(port);
 import queryGasPrice from './blockchain/gasPriceService';
 import { queryEthConversion } from './services/ethconversion/getEthConversionService';
