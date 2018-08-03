ea3a144b7b08fc85d5eebba875ff61cfe2e7c3ef
Michael Teixeira <meixeira@gmail.com>
2018-08-02 21:09:02 -0400

    Re-introduce challenges service and remove old service references

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,6 @@
-const dacs = require('./dacs/dacs.service.js');
-const milestones = require('./milestones/milestones.service.js');
-const campaigns = require('./campaigns/campaigns.service.js');
 const pools = require('./pools/pools.service.js');
 const users = require('./users/users.service.js');
 const uploads = require('./uploads/uploads.service.js');
-const donations = require('./donations/donations.service.js');
 const contributions = require('./contributions/contributions.service.js');
 const transactions = require('./transactions/transactions.service.js');
 
@@ -13,27 +9,16 @@ import challenges from './challenges/challenges.service.js';
 const whitelist = require('./whitelist/whitelist.service.js');
 const gasprice = require('./gasprice/gasprice.service.js');
 
-import pledgeAdmins from './pledgeAdmins/pledgeAdmins.service';
-import donationsHistory from './donationsHistory/donationsHistory.service';
-import donationTokens from './donationTokens/donationTokens.service';
-
 const ethconversion = require('./ethconversion/ethconversion.service.js');
 
 module.exports = function() {
   const app = this;
-  app.configure(dacs);
-  app.configure(milestones);
-  app.configure(campaigns);
   app.configure(pools);
   app.configure(users);
   app.configure(uploads);
-  app.configure(donationsHistory);
-  app.configure(donations);
   app.configure(contributions);
   app.configure(transactions);
   app.configure(challenges);
-  app.configure(pledgeAdmins);
-  app.configure(donationTokens);
   app.configure(whitelist);
   app.configure(gasprice);
   app.configure(ethconversion);
