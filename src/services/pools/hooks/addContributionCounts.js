c4d58dc59b81041cda1d56e31a1974b09cbf0810
Michael Teixeira <meixeira@gmail.com>
2018-08-21 09:47:28 -0400

    Redo addContributionCounts • ignore all contributions that are in pending_confirmation state

--- src/services/pools/hooks/addContributionCounts.js
+++ src/services/pools/hooks/addContributionCounts.js
@@ -1,11 +1,24 @@
 import errors from 'feathers-errors';
+import logger from 'winston';
 export default async context => {
   try {
-    const pools = context.result.data;
-    
+    const pools = context.result;
+    console.log('context.params.query', context.params.query);
+    console.log('context.result', context.result);
+    console.log('pools', pools);
     const poolsWithCounts = await Promise.all(
       pools.map(async (pool) => {
-        const { total: count } = await context.app.service('contributions').find({ query: { $limit: 0, pool: pool._id }});
+        const { total: count } = await context.app.service('contributions')
+          .find({
+            query: {
+              $limit: 0,
+              status: {
+                $ne: 'pending_confirmation'
+              },
+              pool: pool._id
+            }
+          });
+        console.log('count', count);
         pool.contributionCount = count;
         return pool;
       })
