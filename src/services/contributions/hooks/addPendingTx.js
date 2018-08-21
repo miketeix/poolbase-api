9995b1d11f226d3122481b03220e48a614bb541e
Michael Teixeira <meixeira@gmail.com>
2018-08-21 09:50:09 -0400

    Improve Contribution addPendingTx hook

--- src/services/contributions/hooks/addPendingTx.js
+++ src/services/contributions/hooks/addPendingTx.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 import errors from 'feathers-errors';
-import { setByDot } from 'feathers-hooks-common';
-import { soliditySha3, hexToNumber, toWei } from 'web3-utils';
+import { checkContext, setByDot } from 'feathers-hooks-common';
+import { soliditySha3, hexToNumber, toWei, toBN } from 'web3-utils';
 import { estimateGas, getFunctionAbiByName } from '../../../utils/blockchain';
 
 import poolbaseAbi from '../../../blockchain/contracts/PoolbaseAbi.json';
@@ -11,7 +11,9 @@ export default async context => {
   const { poolbaseSignerAddress, nodeUrl } = context.app.get('blockchain');
   const web3 = new Web3(nodeUrl);
 
-  const { status, ownerAddress, poolAddress, amount: contributionAmount } = context.data; // grab poolId or poolAddress
+  const { status, ownerAddress, pool: poolId, amount: contributionAmount } = context.data; // grab poolId or poolAddress
+
+  const { contractAddress: poolAddress } = await context.app.service('pools').get(poolId);
 
   let functionName;
   switch (status) {
@@ -30,16 +32,27 @@ export default async context => {
   }
 
   const functionAbi = getFunctionAbiByName(poolbaseAbi, functionName);
-
+  console.log('poolAddress', poolAddress);
+  console.log('ownerAddress', ownerAddress);
+  console.log('contributionAmount', contributionAmount);
+  console.log('typeof contributionAmount', typeof contributionAmount);
+  
   const hash = soliditySha3(poolAddress, ownerAddress);
   const signature = await web3.eth.sign(hash, poolbaseSignerAddress);
-
   let amount = 0;
   if (status === 'pending_confirmation') {
     amount = toWei(toBN(contributionAmount));
+    console.log('amount', amount);
+    console.log('typeof amount', typeof amount);
   }
+  console.log('functionAbi', functionAbi);
   const data = web3.eth.abi.encodeFunctionCall(functionAbi, [signature]);
-  const gasLimit = await estimateGas(web3, functionName, [signature]);
+  let gasLimit;
+  try {
+    gasLimit = await estimateGas(web3, poolbaseAbi, poolAddress, functionName, [signature]);
+  } catch (estimateGasError) {
+    console.log('estimateGasError', estimateGasError);
+  }
 
   const pendingTx = {
     toAddress: poolAddress,
