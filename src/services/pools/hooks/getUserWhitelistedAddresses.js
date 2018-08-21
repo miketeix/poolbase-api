ff6504d1a3bd02a963e1be934a24f191ff1baa19
Michael Teixeira <meixeira@gmail.com>
2018-08-21 09:45:40 -0400

    fix getUserWhitelistedAddresses hook

--- src/services/pools/hooks/getUserWhitelistedAddresses.js
+++ src/services/pools/hooks/getUserWhitelistedAddresses.js
@@ -1,10 +1,12 @@
+import intersection from 'lodash.intersection';
+
 export default async context => {
   try {
     const { wallets } = await context.app
       .service('users')
       .get(context.params.query.userWhitelisted);
     const walletAddresses = wallets.map(({ address }) => address);
-    const { whitelist } = context.result;
+    const { hasWhitelist, whitelist } = context.result;
     context.result = intersection(walletAddresses, whitelist);
     return context;
   } catch (err) {
