b456d1525c3414b83c2ebddd19830d2fad3fe05d
Michael Teixeira <meixeira@gmail.com>
2018-09-04 15:44:41 -0400

    WIP Add Fees Model/Service to Mongo

--- src/models/fees.model.js
+++ src/models/fees.model.js
@@ -1,12 +1,19 @@
-const NeDB = require('nedb');
-const path = require('path');
 
-module.exports = function(app) {
-  const dbPath = app.get('nedb');
-  const Model = new NeDB({
-    filename: path.join(dbPath, 'fees.db'),
-    autoload: true,
+// fee-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const fee = new Schema({
+    id: { type: String, required: true, index: true, unique: true },
+    type: { type: String, required: true },
+    percent: { type: Number, required: true },
+  }, {
+    timestamps: true
   });
 
-  return Model;
-};
+  return mongooseClient.model('fee', fee);
+
+ };
