68c9903bf538ae7de45675b9c84bc3a86cbdd6b4
Michael Teixeira <meixeira@gmail.com>
2018-09-13 00:10:55 -0400

    WIP

--- src/blockchain/index.js
+++ src/blockchain/index.js
@@ -1,3 +1,35 @@
+import PoolBaseMonitor from './PoolBaseMonitor';
+const eventHandler = require('./lib/eventHandler');
+const { getWeb3 } = require('./lib/web3Helpers');
+
+module.exports = function init() {
+  const app = this;
+
+  const web3 = getWeb3(app);
+  app.getWeb3 = getWeb3.bind(null, app);
+
+  // initialize the event listeners
+  // const handler = eventHandler(app);
+
+  const poolBaseMonitor = PoolBaseMonitor(app);
+  poolBaseMonitor.start();
+
+  web3.on(web3.DISCONNECT_EVENT, () => {
+    poolBaseMonitor.close();
+  });
+
+  web3.on(web3.RECONNECT_EVENT, () => {
+    // web3.setProvider will clear any existing subscriptions, so we need to re-subscribe
+    poolBaseMonitor.start();
+  });
+};
+
+
+
+
+
+
+
 import Web3 from 'web3';
 import logger from 'winston';
 
@@ -6,25 +38,21 @@ import poolbaseFactoryAbi from './contracts/PoolbaseFactoryAbi.json';
 import poolbaseEventEmitterAbi from './contracts/PoolbaseEventEmitterAbi.json';
 import poolbaseAbi from './contracts/PoolbaseAbi.json';
 
-// import LiquidPledgingMonitor from './LiquidPledgingMonitor';
-// import FailedTxMonitor from './FailedTxMonitor';
-// import { LiquidPledging, LPVault } from 'giveth-liquidpledging-token';
-// import { LPPCappedMilestones } from 'lpp-capped-milestone-token';
-// import { LPPDacs } from 'lpp-dacs';
+const { getWeb3 } = require('./lib/web3Helpers');
 
 const ONE_MINUTE = 60 * 1000;
 
 export default function() {
   const app = this;
   const blockchain = app.get('blockchain');
+  // { startingBlock, nodeUrl, poolFactoryAddress, eventEmitterAddress }
 
-  const web3 = new Web3(blockchain.nodeUrl);
+  const web3 = getWeb3(app)//new Web3(blockchain.nodeUrl);
 
   const opts = {
     startingBlock: blockchain.startingBlock,
   };
 
-  let failedTxMonitor;
   let poolbaseMonitor;
 
   // initialize the event listeners
@@ -37,22 +65,9 @@ export default function() {
     const [coinbaseAccount] = await web3.eth.getAccounts();
     console.log('coinbaseAccount', coinbaseAccount);
 
-    // txMonitor = new FailedTxMonitor(web3, app);
-    // txMonitor.start();
-
-    // TODO investigate this
-    // for some reason, if we have the contracts in getNetwork as in commit #67196cd807c52785367aee5224e8d6e5134015c8
-    // upon reconnection, the web3 provider will not update and will throw "connection not open on send()"
-    // maybe https://github.com/ethereum/web3.js/issues/1188 is the issue?
-    const contractOptions = {
-      from: coinbaseAccount,
-      gasPrice: '20000000000',
-    };
-
     const poolbaseFactory = await new web3.eth.Contract(
       poolbaseFactoryAbi,
       blockchain.poolFactoryAddress,
-      contractOptions,
     );
 
     const poolbaseEventEmitter = await new web3.eth.Contract(
