ac293043ab9d3873c69c95cad926b324f46a6c95
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-09-28 16:42:57 +0200

    fix gas estimation error for cloned poolbase contract calls

--- src/services/contributions/hooks/addPendingTx.js
+++ src/services/contributions/hooks/addPendingTx.js
@@ -1,28 +1,27 @@
 import Web3 from 'web3';
-import errors from 'feathers-errors';
 import { checkContext, setByDot } from 'feathers-hooks-common';
 import AWS from 'aws-sdk';
-import { soliditySha3, hexToNumber, toWei, toBN } from 'web3-utils';
+import { soliditySha3, toWei } from 'web3-utils';
 import { estimateGas, getFunctionAbiByName } from '../../../utils/blockchain';
 
 import poolbaseAbi from '../../../blockchain/contracts/PoolbaseAbi.json';
-
-
+import poolbaseFactoryAbi from '../../../blockchain/contracts/PoolbaseFactoryAbi.json';
 
 export default async context => {
   checkContext(context, 'before', ['patch', 'create']);
-  const { poolbaseSignerAddress, nodeUrl, keystorePassphrase } = context.app.get('blockchain');
+  const { nodeUrl, keystorePassphrase } = context.app.get('blockchain');
   const { bucketName, secretsPath, accessKey, secretAccessKey } = context.app.get('aws');
 
   const s3 = new AWS.S3({
     region: 'eu-central-1',
     accessKeyId: accessKey,
-    secretAccessKey: secretAccessKey
+    secretAccessKey,
   });
 
   const web3 = new Web3(nodeUrl);
 
   const { status, ownerAddress, poolAddress, amount: contributionAmount } = context.data;
+  const { poolFactoryAddress } = context.app.get('blockchain');
 
   let functionName;
   switch (status) {
@@ -42,16 +41,17 @@ export default async context => {
 
   const functionAbi = getFunctionAbiByName(poolbaseAbi, functionName);
 
-  const getAwsResponse = await s3.getObject({
+  const getAwsResponse = await s3
+    .getObject({
       Bucket: bucketName,
-      Key: `${secretsPath}/keystore.json`
-    }).promise();
+      Key: `${secretsPath}/keystore.json`,
+    })
+    .promise();
   const rawKeystoreFile = JSON.parse(getAwsResponse.Body);
   const decrypted = web3.eth.accounts.decrypt(rawKeystoreFile, keystorePassphrase);
 
   const hash = soliditySha3(poolAddress, ownerAddress);
   const { signature } = web3.eth.accounts.sign(hash, decrypted.privateKey, true);
-
 
   let amount = 0;
   if (status === 'pending_confirmation') {
@@ -59,14 +59,20 @@ export default async context => {
   }
   console.log('amount', amount);
   console.log('typeof amount', typeof amount);
-  
+
+  // get poolbase library address from poolbaseCloneFactory
+  const poolbaseFactory = new web3.eth.Contract(poolbaseFactoryAbi, poolFactoryAddress);
+  const poolbaseLibraryAddress = await poolbaseFactory.libraryAddress();
+
   const data = web3.eth.abi.encodeFunctionCall(functionAbi, [signature]);
   let gasLimit;
   try {
-    console.log('poolAddress', poolAddress);
+    console.log('poolbaseLibraryAddress', poolbaseLibraryAddress);
     console.log('functionName', functionName);
     console.log('signature', signature);
-    gasLimit = await estimateGas(web3, poolbaseAbi, poolAddress, functionName, [signature]);
+    gasLimit = await estimateGas(web3, poolbaseAbi, poolbaseLibraryAddress, functionName, [
+      signature,
+    ]);
   } catch (estimateGasError) {
     console.log('estimateGasError', estimateGasError);
   }
@@ -75,8 +81,8 @@ export default async context => {
     toAddress: poolAddress,
     amount,
     gasLimit,
-    data
-  }
+    data,
+  };
 
   setByDot(context.data, 'pendingTx', pendingTx);
   return context;
